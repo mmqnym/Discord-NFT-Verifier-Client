f1dffc5962186336cff454ad488fc12445205884
0xmimiQ <0xmimiq@gmail.com>
2022-08-16 17:23:59 +0800

    client-end init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
