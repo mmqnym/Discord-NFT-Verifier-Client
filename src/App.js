fb9732377b741ec157335656949f7d666e1b9145
0xmimiQ <0xmimiq@gmail.com>
2022-08-23 04:19:30 +0800

    done wallet connection.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,71 @@
 import "./App.css";
 import title from "./cronos_kaiju.png";
-
+import React, { useState, useEffect } from "react";
+import CryptoModal from "./component/Modals"
 
 function App() {
-  const redirectClick = () => {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1008064554183446528&redirect_uri=http%3A%2F%2F124.109.122.99%3A11112&response_type=code&scope=identify";
-  }
 
+  const [discordUser, setDiscordUser] = useState({});
+  // var discordUserAvatar = "";
+
+  useEffect(() => {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+
+    const fetchUsers = () => {
+        fetch("https://discord.com/api/users/@me", {
+          headers: {
+            authorization: `${tokenType} ${accessToken}`,
+          },
+        })
+        .then(result => result.json())
+        .then(response => {
+          setDiscordUser(response);
+        })
+        .catch(console.error);
+    };
+
+    if (accessToken) {
+      fetchUsers();
+    }
+
+  }, []);
+
+  
+  useEffect(() => {
+    if (discordUser !== {}) {
+      // discordUserAvatar = `https://cdn.discordapp.com/avatars/${discordUser["id"]}/${discordUser["avatar"]}`
+      console.log(discordUser)
+    }
+  }, [discordUser]);
+
+  const [showModal, setShowModal] = useState(false);
+  const handleModalColse = () => setShowModal(false);
 
   return (
     <div className="Cronos-Kaiju">
-      <header className="Verify-Page">
-        <div className="Verify-Page-bg bg-center w-full h-[100vh] flex flex-col gap-40 justify-center items-center">
-          <div className="Verify-Page-rain bg-center w-full h-[100vh] flex flex-col gap-40 justify-center items-center">
-            <img src={title} alt="title" />
+      <div className="Verify-Page-bg bg-center w-full h-[100vh] flex flex-col gap-40 justify-center items-center">
+        <div className="Verify-Page-rain bg-center w-full h-[100vh] flex flex-col gap-40 justify-center items-center">
+          <img src={title} alt="title" />
+          <button id="connectBtn" className="relative inline-flex items-center justify-center h-16 p-1 mb-2 mr-2 overflow-hidden sm:text-xl font-mono font-medium 
+          rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 
+          shadow hover:shadow-lg focus:ring-4 focus:outline-none focus:ring-blue-300 active:translate-y-1" 
+          type="button" onClick={() => setShowModal(true)}>
+          
+            <svg aria-hidden="true" className="ml-1 mr-1 w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 332" >
+              <path d="m126.613 93.9842c62.622-61.3123 164.152-61.3123 226.775 0l7.536 7.3788c3.131 3.066 3.131 8.036 0 11.102l-25.781 25.242c-1.566 1.533-4.104 1.533-5.67 0l-10.371-10.154c-43.687-42.7734-114.517-42.7734-158.204 0l-11.107 10.874c-1.565 1.533-4.103 1.533-5.669 0l-25.781-25.242c-3.132-3.066-3.132-8.036 0-11.102zm280.093 52.2038 22.946 22.465c3.131 3.066 3.131 8.036 0 11.102l-103.463 101.301c-3.131 3.065-8.208 3.065-11.339 0l-73.432-71.896c-.783-.767-2.052-.767-2.835 0l-73.43 71.896c-3.131 3.065-8.208 3.065-11.339 0l-103.4657-101.302c-3.1311-3.066-3.1311-8.036 0-11.102l22.9456-22.466c3.1311-3.065 8.2077-3.065 11.3388 0l73.4333 71.897c.782.767 2.051.767 2.834 0l73.429-71.897c3.131-3.065 8.208-3.065 11.339 0l73.433 71.897c.783.767 2.052.767 2.835 0l73.431-71.895c3.132-3.066 8.208-3.066 11.339 0z"/>
+            </svg>
+            <span className="relative h-14 px-3 py-4 dark:bg-gray-900 dark:text-white bg-gray-900 text-white hover:text-gray-900 hover:font-semibold transition-all ease-in duration-75 rounded-md group-hover:bg-opacity-0">
+              Connect Wallet
+            </span>
+          </button>
 
-            <button onClick={redirectClick} id="redirectBtn" className="relative inline-flex items-center justify-center h-16 p-1 mb-2 mr-2 overflow-hidden 
-            sm:text-xl font-mono font-medium 
-            rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 
-            shadow hover:shadow-lg focus:ring-4 focus:outline-none focus:ring-blue-300 active:translate-y-1">
-            
-              <svg aria-hidden="true" className="ml-2.5 mr-2.5 w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 71 55" >
-                <path d="M60.1045 4.8978C55.5792 2.8214 50.7265 1.2916 45.6527 0.41542C45.5603 0.39851 45.468 0.440769 45.4204 0.525289C44.7963 1.6353 44.105 3.0834 43.6209 4.2216C38.1637 3.4046 32.7345 3.4046 27.3892 4.2216C26.905 3.0581 26.1886 1.6353 25.5617 0.525289C25.5141 0.443589 25.4218 0.40133 25.3294 0.41542C20.2584 1.2888 15.4057 2.8186 10.8776 4.8978C10.8384 4.9147 10.8048 4.9429 10.7825 4.9795C1.57795 18.7309 -0.943561 32.1443 0.293408 45.3914C0.299005 45.4562 0.335386 45.5182 0.385761 45.5576C6.45866 50.0174 12.3413 52.7249 18.1147 54.5195C18.2071 54.5477 18.305 54.5139 18.3638 54.4378C19.7295 52.5728 20.9469 50.6063 21.9907 48.5383C22.0523 48.4172 21.9935 48.2735 21.8676 48.2256C19.9366 47.4931 18.0979 46.6 16.3292 45.5858C16.1893 45.5041 16.1781 45.304 16.3068 45.2082C16.679 44.9293 17.0513 44.6391 17.4067 44.3461C17.471 44.2926 17.5606 44.2813 17.6362 44.3151C29.2558 49.6202 41.8354 49.6202 53.3179 44.3151C53.3935 44.2785 53.4831 44.2898 53.5502 44.3433C53.9057 44.6363 54.2779 44.9293 54.6529 45.2082C54.7816 45.304 54.7732 45.5041 54.6333 45.5858C52.8646 46.6197 51.0259 47.4931 49.0921 48.2228C48.9662 48.2707 48.9102 48.4172 48.9718 48.5383C50.038 50.6034 51.2554 52.5699 52.5959 54.435C52.6519 54.5139 52.7526 54.5477 52.845 54.5195C58.6464 52.7249 64.529 50.0174 70.6019 45.5576C70.6551 45.5182 70.6887 45.459 70.6943 45.3942C72.1747 30.0791 68.2147 16.7757 60.1968 4.9823C60.1772 4.9429 60.1437 4.9147 60.1045 4.8978ZM23.7259 37.3253C20.2276 37.3253 17.3451 34.1136 17.3451 30.1693C17.3451 26.225 20.1717 23.0133 23.7259 23.0133C27.308 23.0133 30.1626 26.2532 30.1066 30.1693C30.1066 34.1136 27.28 37.3253 23.7259 37.3253ZM47.3178 37.3253C43.8196 37.3253 40.9371 34.1136 40.9371 30.1693C40.9371 26.225 43.7636 23.0133 47.3178 23.0133C50.9 23.0133 53.7545 26.2532 53.6986 30.1693C53.6986 34.1136 50.9 37.3253 47.3178 37.3253Z"/>
-              </svg>
-              <span className="relative h-14 px-3 py-3.5 dark:bg-gray-900 dark:text-white bg-gray-900 text-white hover:text-gray-900 hover:font-semibold transition-all ease-in duration-75  rounded-md group-hover:bg-opacity-0">
-                Login with Discord
-              </span>
-            </button>
-          </div>
+          <CryptoModal visible={showModal} onClose={handleModalColse} />
         </div>
-        
-      </header>
+      </div>
     </div>
+
+    
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './loginPage.js';
 
 test('renders learn react link', () => {
   render(<App />);
